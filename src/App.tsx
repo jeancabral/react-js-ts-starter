e0c0a12322519269852e9c93992f0045861c73f0
Jean Cabral <jeancabral@gmail.com>
2020-08-22 17:36:46 -0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
-    <h1>ReactJS Starter com TS by Jeazinho</h1>
-  </>
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home: React.FC = () => (
+  <div>
+    <h1>ReactJS Starter com TS by Jeazinho</h1>
+  </div>
+);
+
+export default Home;
